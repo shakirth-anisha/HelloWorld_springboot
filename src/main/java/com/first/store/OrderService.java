a7299845fd3151f86d3c4c6d2c93e084f2779a03
shakirth-anisha <shakirthanisha@gmail.com>
2025-04-02 17:55:56 +0530

    making beans? (objects managed by spring)

--- src/main/java/com/first/store/OrderService.java
+++ src/main/java/com/first/store/OrderService.java
@@ -9,4 +9,8 @@ public class OrderService {
     public void placeOrder(){
         paymentService.processPayment(10);
     }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 }
--- src/main/java/com/first/store/StoreApplication.java
+++ src/main/java/com/first/store/StoreApplication.java
@@ -2,13 +2,14 @@ package com.first.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(StoreApplication.class, args);
-		var orderService = new OrderService(new PayPalPaymentService());
+		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		var orderService = context.getBean(OrderService.class);
 		orderService.placeOrder();
 	}
 
